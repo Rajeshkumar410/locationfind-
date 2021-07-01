a72bae98da5686e6e8fcf2710ab75dba3bc70b21
Rajeshkumar410 <86779820+Rajeshkumar410@users.noreply.github.com>
2021-07-01 17:00:21 +0530

    Add files via upload
    findlocation workshop

---
+++ MainActivity.java
@@ -0,0 +1,41 @@
+package com.nextstacks.locationfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import java.security.Permission;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.mapframe, mapFragment).commit();
+        mapFragment.getMapAsync(this);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 135);
+        }
+
+
+        @Override
+        public void onMapReady (GoogleMap googleMap); {
+
+    }
